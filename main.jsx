beae877cff96369e8b56b7e8ad3152a209f4db24
yesilin <1211696688@qq.com>
2019-12-19 13:33:55 +0800

    第一次提交！

---
+++ main.jsx
@@ -0,0 +1,52 @@
+// Select all layers (选择全部图层)
+function selectAllLayers() {
+    var desc29 = new ActionDescriptor();
+    var ref23 = new ActionReference();
+    ref23.putEnumerated(charIDToTypeID('Lyr '), charIDToTypeID('Ordn'), charIDToTypeID('Trgt'));
+    desc29.putReference(charIDToTypeID('null'), ref23);
+    executeAction(stringIDToTypeID('selectAllLayers'), desc29, DialogModes.NO);
+}
+
+
+// Set background color hex version (设置背景颜色 hex 版)
+// Example: setBackgroundColorHex("FFFFFF")
+function setBackgroundColorHex(value) {
+    var color = new SolidColor();
+    color.rgb.hexValue = value;
+    app.backgroundColor = color;
+}
+
+
+// Set background color RGB version (设置背景颜色 RGB 版)
+// Example: setBackgroundColorHex(255, 255, 255)
+function setBackgroundColorRGB(r, g, b) {
+    var c = new SolidColor;
+    c.rgb.red = r;
+    c.rgb.green = g;
+    c.rgb.blue = b;
+    app.backgroundColor = c;
+}
+
+
+// Back to previous level history (返回上一级历史)
+function returnPreviousHistory() {
+    var idslct = charIDToTypeID("slct");
+    var idnull = charIDToTypeID("null");
+    var idHstS = charIDToTypeID("HstS");
+    var idOrdn = charIDToTypeID("Ordn");
+    var idPrvs = charIDToTypeID("Prvs");
+    var ref = new ActionReference();
+    var desc = new ActionDescriptor();
+    ref.putEnumerated(idHstS, idOrdn, idPrvs);
+    desc.putReference(idnull, ref);
+    executeAction(idslct, desc, DialogModes.NO);
+}
+
+
+
+
+
+
+
+
+
